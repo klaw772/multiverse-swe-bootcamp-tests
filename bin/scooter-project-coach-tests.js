731dafbec4b99ed9e000bef26c09710610408f85
Kat Law <kathleen.law@multiverse.io>
2023-04-22 16:58:32 -0400

    adjust paths

--- bin/scooter-project-coach-tests.js
+++ bin/scooter-project-coach-tests.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 console.log(__dirname);
 
 // destination will be created or overwritten by default.
-fs.cp('../scooter-project-coach-tests', '../..', { recursive: true }, (err) => {
+fs.cp('node_modules/multiverse-swe-bootcamp-tests/scooter-project-coach-tests', 'scooter-project-coach-tests', { recursive: true }, (err) => {
   if (err) throw err;
   console.log('File was copied to destination');
 });
