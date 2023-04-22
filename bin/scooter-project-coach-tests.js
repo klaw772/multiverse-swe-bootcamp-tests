cbf6e2629aba587552d2ba1dd177ad4de779e9c9
Kat Law <kathleen.law@multiverse.io>
2023-04-22 16:44:07 -0400

    please

--- bin/scooter-project-coach-tests.js
+++ bin/scooter-project-coach-tests.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 console.log('hello');
 
 // destination will be created or overwritten by default.
-fs.copyFile('../scooter-project-coach-tests', '../..', (err) => {
+fs.cp('../scooter-project-coach-tests', '../..', { recursive: true }, (err) => {
   if (err) throw err;
   console.log('File was copied to destination');
 });
