bf3a32ab159e7c0980481f50f1389e0986a3f23c
Kat Law <kathleen.law@multiverse.io>
2023-04-22 15:37:07 -0400

    adjust copy directory script

--- bin/scooter-project-coach-tests.js
+++ bin/scooter-project-coach-tests.js
@@ -1,3 +1,9 @@
 #!/usr/bin/env node
+const fs = require('fs');
+console.log('hello');
 
-console.log('hello')
+// destination will be created or overwritten by default.
+fs.copyFile('../scooter-project-coach-tests', '../..', (err) => {
+  if (err) throw err;
+  console.log('File was copied to destination');
+});
